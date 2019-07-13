93b7659cc01fc0878abb77e2ec329d5bfd0765b0
JamesArnold <james@transactionwarehouse.net>
2019-07-13 14:13:09 -0500

    main menu setup

--- css-grid-demo/src/app/main-menu/main-menu.component.ts
+++ css-grid-demo/src/app/main-menu/main-menu.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { examples } from '../examples';
+
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
 export class MainMenuComponent implements OnInit {
+
+  examples = examples;
 
   constructor() { }
 
